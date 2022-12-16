88197e813f2e13a0994cad36209a67ae9e3fc62e
Abhishek <abhishekshakya98.com@gmail.com>
2022-12-16 09:09:21 +0545

    added animation

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,29 @@
 function sidebar_toggle() {
     var x = document.getElementById('top_sidebar');
     if(x.className === "topnav") {
-        x.className += " sidebar_active";
+        x.classList.add("sidebar_active");
     } else {
-        x.className = 'topnav';
+        x.classList.remove("sidebar_active");
     }
 }
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
