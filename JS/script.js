b6848284a708b4cf949b8546d04dba64e668dc2a
Abhishek <abhishekshakya98.com@gmail.com>
2022-12-17 08:39:00 +0545

    made hte page responsive

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,28 @@
-function sidebar_toggle() {
+window.addEventListener('load', () => {
+  const scrollDown = document.querySelector('.scrollFullHeight');
+  const a = window.innerHeight;
+  
+
+  function sidebar_toggle() {
     var x = document.getElementById('top_sidebar');
-    if(x.className === "topnav") {
-        x.classList.add("sidebar_active");
+    if (x.className === "topnav") {
+      x.classList.add("sidebar_active");
     } else {
-        x.classList.remove("sidebar_active");
+      x.classList.remove("sidebar_active");
     }
-}
+  }
 
 
-function reveal() {
+  function reveal() {
     var reveals = document.querySelectorAll(".reveal");
     for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
+      var windowHeight = innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
-        reveals[i].classList.remove("active");
+        // reveals[i].classList.remove("active");
       }
     }
   }
@@ -25,5 +30,14 @@ function reveal() {
 
   window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
-reveal();
+  // To check the scroll position on page load
+  reveal();
+
+  function scrollFullHeight() {
+    document.body.scrollTop = a;
+    document.documentElement.scrollTop = a;
+  }
+
+  scrollDown.addEventListener('click', scrollFullHeight);
+})
+
